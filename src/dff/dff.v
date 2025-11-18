c5499e13223964b2ea6de266039e81686a573db5
Adrien Abbey <abbey.12@wright.edu>
2025-11-17 19:54:26 -0500

    updated comments

--- src/dff/dff.v
+++ src/dff/dff.v
@@ -27,23 +27,23 @@
 -------------------------------------------------------------------------------
 */
 
-module dff #(
-    parameter DATA_WIDTH = 8
+module dff #(  // The # means this is a parameterized module.
+    parameter DATA_WIDTH = 8  // Default to 8 bits, can be changed.
 ) (
     input                       clk,       // Clock input
     input                       reset_n,   // Active-low reset input
     input                       enable_n,  // Active-low enable input
-    input      [DATA_WIDTH-1:0] d,         // Input bus
-    output reg [DATA_WIDTH-1:0] q          // Output bus
+    input      [DATA_WIDTH-1:0] d,         // Input bus, adjusted to parameter size
+    output reg [DATA_WIDTH-1:0] q          // Output bus as a register, adjusted to parameter size
 );
 
   always @(negedge reset_n) begin  // As soon as reset is asserted...
-    q <= 0;  // Reset the output to zero.
+    q <= 0;  // Reset the output to zero, non-blocking.
   end
 
   always @(posedge clk) begin  // On the rising clock edge...
     if (enable_n == 0 && reset_n == 1) begin  // If enable is asserted but reset is not...
-      q <= d;  // Assign the input to the output.
+      q <= d;  // Assign the input to the output, non-blocking.
     end
   end
 
